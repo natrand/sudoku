6c0f0f20094e0a29c7757319afd26d70860a268f
nszuper <szupernatalia@gmail.com>
2024-02-28 00:15:56 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(600, 800), "SUDOKU");
+    sf::RectangleShape shape(sf::Vector2f(200, 200));
+    shape.setFillColor(sf::Color::Red);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
